2618df82f2765405645a3bf4b87292b3b2296f7d
Matthew Grainger <matthew.grainger@nina.no>
2020-05-30 13:28:31 +0200

    Add function skeleton

---
+++ R/get_data_metadata.R
@@ -0,0 +1,13 @@
+#' Functions for mapping to Darwin Core
+#' function for adding data level metadata to the (e.g. institution, country, community, etc)
+#' BORROW FROM EML??
+#' @param ??
+#' @return data level metadata in eml
+#' @export
+#'
+
+
+get_data_metadata<-function(){
+
+
+}
---
+++ R/get_temporal_cover.R
@@ -0,0 +1,11 @@
+#' get_temporal_cover
+#' Get temporal coverage from raw data / mapped data
+#' @param x Date/time column from user defined dataframe
+#' @return Output: temporal extent chart?
+#' @export
+
+
+get_temporal_cover<-function(x){
+
+
+}
