594ed8197f24d8abb26ee877b19e20859556e844
Matthew Grainger <matthew.grainger@nina.no>
2020-06-01 13:33:04 +0200

    Add get_files

--- R/get_files.R
+++ R/get_files.R
@@ -6,6 +6,13 @@
 
 
 get_files<-function(project_name){
+  path<-paste0(getwd(),"/", project_name)
+  dirslist<-list.dirs(path,recursive = TRUE)
+  x <- lapply(strsplit(dirslist, "/"), function(z) as.data.frame(t(z)))
+  LivingNorway_project <- plyr::rbind.fill(x)
+  y=dim(LivingNorway_project)[2]
+  p <- collapsibleTree::collapsibleTree( LivingNorway_project, c("V8", "V9"))
+  p
+  }
 
 
-}
