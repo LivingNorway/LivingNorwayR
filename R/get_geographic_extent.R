eec19d4be89b3f55af88d0260a52a28476a0874f
Matthew Grainger <matthew.grainger@nina.no>
2020-06-09 16:13:12 +0200

    edit functions

--- R/get_geographic_extent.R
+++ R/get_geographic_extent.R
@@ -21,3 +21,24 @@ get_geographic_extent<-function(lat,lon){
 }
 
 
+# get Norway geographic extent
+#' Get the geographic extent of a dataframe with geographic UTM XY coordinates
+#'
+#' This function plots a map of the UTM X Y coordinates from a datafile specified by the user.
+#' @param df the dataframe
+#' @param X latitude column indicated by data$X
+#' @param Y longitude column indicated by data$Y
+#' @param Code Code to convert UTM to lat lon
+#' @return A ggplot map of the coordinates
+#' @example
+#' @export
+
+get_NOR_geographic_extent<-function(df,X,Y,Code){
+  df.sf <- sf::st_as_sf(df, coords = c(X, Y) ) %>%
+    sf::st_set_crs(Code) %>%   #set coordinate system used
+    sf::st_transform(4326)     #transform coordinates to WGS84 coordinates
+  leaflet::leaflet() %>% addTiles() %>% addMarkers( data = df.sf )
+}
+
+
+
