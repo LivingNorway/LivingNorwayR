47bccea6d23e6b1cd56fd82eb724935bf68e2e72
Joseph Chipperfield <joechip90@googlemail.com>
2020-07-09 09:57:35 +0200

    Example of method Just a quick function definition (part-finished archive export function) that illustrates how to add methods to classes

--- R/DwCClassLibrary/DwCArchive.R
+++ R/DwCClassLibrary/DwCArchive.R
@@ -3,5 +3,12 @@ setRefClass(
   Class = "DwCArchive",
   fields = list(
     componentList = "list"
+  ),
+  methods = list(
+    # Function to export the object to a Darwin core archive file structure
+    exportToArchive = function(loc) {
+      # Retrieve a list of data types in DwCArchive object
+      obTypes <- unique(sapply(X = componentList, FUN = class))
+    }
   )
 )
