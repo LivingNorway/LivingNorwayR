5137051f5dd54bafe682d489dd41f44b1f3fc102
Matthew Grainger <matthew.grainger@nina.no>
2020-07-27 14:42:30 +0200

    EMLD make metadata

---
+++ R/read_metadata.R
@@ -0,0 +1,15 @@
+#' read_metadata
+#' read the metadata from a rmarkdown file
+#' @param X an RMarkdown file
+#' @return Output: text string of yaml information
+#' @export
+
+read_metadata = function(file) {
+  x = readr::read_lines(file) # read markdown using readlines
+  rng = grep("^---$", x)
+  rng = rng + c(1, -1)
+  x = x[rng[1]:rng[2]]
+  names(x) = gsub("(.*):.*", "\\1", x)
+  x = gsub(".*: (.*)", "\\1", x)
+  as.list(x)
+}
