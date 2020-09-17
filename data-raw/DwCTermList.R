9d80cbe07afaaf6ca6e5a6aa3e7edbfa19444deb
Joseph Chipperfield <joechip90@googlemail.com>
2020-09-17 21:20:05 +0200

    DwCTerm Added a DwCTerm class specification to ease the handling of registered Darwin core terms and to make it easier for users to specifiy their own terms (so that registered extensions can be supported by the package)

--- data-raw/DwCTermList.R
+++ data-raw/DwCTermList.R
@@ -1,6 +1,6 @@
 # Source file to generate the data frame of Darwin core terms used by some package functions
 # ------ 1. RETRIEVE TERM SPECIFICATIONS AND REGISTER AS DATA ------
 # Retrieve the term specifications
-DwCTermFrame <- retrieveDwCTermSpecifications(FALSE, TRUE)
+DwCTermList <- retrieveDwCTermSpecifications(FALSE, TRUE)
 # Store the retrieved term specifications as data in the package
-usethis::use_data(DwCTermFrame, overwrite = TRUE, internal = TRUE)
+usethis::use_data(DwCTermList, overwrite = TRUE, internal = TRUE)
