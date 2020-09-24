5bec0c6cc0244c169d08ddbb1037e80a739c3933
Matthew Grainger <matthew.grainger@nina.no>
2020-09-24 12:59:02 +0200

    get_metadata()

--- R/get_data_metadata.R
+++ R/get_data_metadata.R
@@ -1,13 +1,20 @@
-#' Functions for mapping to Darwin Core
-#' function for adding data level metadata to the (e.g. institution, country, community, etc)
-#' BORROW FROM EML??
-#' @param ??
-#' @return data level metadata in eml
+#' get_data_metadata
+#' read the metadata from a rmarkdown file
+#' @param X an RMarkdown file
+#' @return Output: text string of yaml information
 #' @export
-#'
+
+get_data_metadata = function(file) {
+  x = readr::read_lines(file) # read markdown using readlines
+  rng = grep("^---$", x)
+  rng = rng + c(1, -1)
+  x = x[rng[1]:rng[2]]
+  names(x) = gsub("(.*):.*", "\\1", x)
+  x = gsub(".*: (.*)", "\\1", x)
+  as.list(x)
+}
 
 
-get_data_metadata<-function(){
 
 
 }
