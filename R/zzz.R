5424d6485a818fb23cbd23f8687956bbc1460cf7
Matthew Grainger <DrMattG@users.noreply.github.com>
2022-03-29 11:28:50 +0200

    updates to namespace

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 #' @export
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to LivingNorwayR for more information about the project see https://livingnorway.no/.
-  If you have any problems please submit an issue at https://github.com/LivingNorway/LivingNorwayR")
+  packageStartupMessage("Welcome to LivingNorwayR for more information
+  about the project see https://livingnorway.no/.
+  If you have any problems please submit an issue
+                        at https://github.com/LivingNorway/LivingNorwayR")
 }
 
